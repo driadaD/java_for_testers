b655fd5c32cb53de8c7370f149a6a3858dcfadde
driadaD <heryvimka17@gmail.com>
2022-01-14 18:23:05 +0700

    лекция 6.2 и 6.3 генератор тестовых данных

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -0,0 +1,57 @@
+package ru.stqa.pft.addressbook.generators;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactDataGenerator {
+
+    @Parameter(names = "-c", description = "Contact count")
+    public int count;
+
+    @Parameter (names = "-f", description = "Target file")
+    public String file;
+
+    public static void main (String [] args) throws IOException {
+        ContactDataGenerator generator = new ContactDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+        try{
+            jCommander.parse(args);
+        } catch (ParameterException ex) {
+            jCommander.usage();
+            return;
+        }
+        generator.run();
+    }
+
+    private void run() throws IOException {
+        List<ContactData> contacts = generateContacts(count);
+        save(contacts, new File(file));
+    }
+
+    private static void save(List<ContactData> contacts, File file) throws IOException {
+        System.out.println(new File(".").getAbsolutePath());
+        Writer writer = new FileWriter(file);
+        for (ContactData contact : contacts){
+            writer.write(String.format("%s;%s;%s;%s\n", contact.getFirstname(), contact.getLastname(),contact.getAddress(),contact.getMobilePhone()));
+        }
+        writer.close();
+    }
+
+    private static List<ContactData> generateContacts(int count) {
+        List<ContactData> contacts = new ArrayList<ContactData>();
+        for (int i = 0; i < count; i++ ){
+            contacts.add(new ContactData().withFirstname(String.format("firstName %s",i)).withLastname(String.format("lastName %s",i)).withAddress("address").withMobilePhone(String.format("mobilePhone %s",i)));
+        }
+        return contacts;
+    }
+}
