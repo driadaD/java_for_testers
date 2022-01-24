f6932e68c313f546a1eeeafb32a9389fc4e1677f
driadaD <heryvimka17@gmail.com>
2022-01-24 22:52:03 +0700

    лекция 8.4 ленивая инициализация

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
@@ -35,7 +35,7 @@ public class HttpSession {
         post.setEntity(new UrlEncodedFormEntity(params));
         CloseableHttpResponse response = httpclient.execute(post);
         String body = geTextFrom(response);
-        return body.contains(String.format("<span class=\"italic\">%s</span", username));
+        return body.contains(String.format("<span class=\"italic\">%s</span>", username));
     }
 
     private String geTextFrom(CloseableHttpResponse response) throws IOException {
@@ -50,6 +50,6 @@ public class HttpSession {
         HttpGet get = new HttpGet(app.getProperty("web.baseUrl") + "/index.php");
         CloseableHttpResponse response = httpclient.execute(get);
         String body = geTextFrom(response);
-        return body.contains(String.format("<span class=\"italic\">%/span>", username));
+        return body.contains(String.format("<span class=\"italic\">%s</span>", username));
     }
 }
