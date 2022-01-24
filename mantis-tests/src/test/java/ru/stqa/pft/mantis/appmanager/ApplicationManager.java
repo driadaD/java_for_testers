ecc9df9d4601ca67a190eadce5ae7fd3f78bce96
driadaD <heryvimka17@gmail.com>
2022-01-24 20:06:35 +0700

    лекция 8.3 взаимодействие с сервером (HTTP)

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -43,4 +43,14 @@ public class ApplicationManager {
     public void stop() {
         wd.quit();
     }
+
+    public HttpSession newSession(){
+        return new HttpSession(this);
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
 }
+
+
