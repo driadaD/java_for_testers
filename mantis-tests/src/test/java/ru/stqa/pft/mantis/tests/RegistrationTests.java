32c641b6704913f62a44dff9a19af5c0d1e27955
driadaD <heryvimka17@gmail.com>
2022-01-26 00:14:50 +0700

    лекция 8.7 получение почты по протоколу Telnet

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -1,6 +1,5 @@
 package ru.stqa.pft.mantis.tests;
 
-import com.sun.xml.internal.messaging.saaj.packaging.mime.MessagingException;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -10,6 +9,7 @@ import ru.stqa.pft.mantis.model.MailMessage;
 
 import java.io.IOException;
 import java.util.List;
+import javax.mail.*;
 
 import static org.testng.Assert.assertTrue;
 
@@ -21,13 +21,15 @@ public class RegistrationTests extends TestBase {
     }
 
     @Test
-    public void testRegistration () throws MessagingException, IOException {
+    public void testRegistration () throws IOException, MessagingException, com.sun.xml.internal.messaging.saaj.packaging.mime.MessagingException {
         long now = System.currentTimeMillis();
         String user = String.format("user%s", now);
         String password = "password";
         String email = String.format("user%s@localhost.localdomain", now);
+        //app.james().createUser(user, password);
         app.registration().start(user, email);
         List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+        //List<MailMessage> mailMessages = app.james().waitForMail(user, password, 60000);
         String confirmationLink = findConfirmationLink(mailMessages, email);
         app.registration().finish(confirmationLink, password);
         assertTrue(app.newSession().login(user, password));
