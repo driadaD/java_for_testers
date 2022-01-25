e1019fa9f268e1af105aebaef122c66ca89f6207
driadaD <heryvimka17@gmail.com>
2022-01-25 22:03:51 +0700

    лекция 8.6 получение почты

--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -5,3 +5,5 @@ $g_database_name = 'bugtracker';
 $g_db_username = 'administrator';
 $g_db_password = 'root';
 $g_signup_use_captcha = OFF;
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'localhost';
